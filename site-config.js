991302db1ee9c31d45f4ba9162c34d98d64e7367
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-09-04 10:26:04 +0900

    Connect scrap site to production management

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.orderAutoIntegration = {
   // 管理システムの問い合わせ受付専用RPCへ接続します。
-  supabaseUrl: "https://wlinebwdmbnbjbyvqrig.supabase.co",
-  supabasePublishableKey: "sb_publishable_298gkO4cyTqi21SwRtLnWQ_1-c5FTJe",
+  supabaseUrl: "https://qdzdskryxwjjwtwigztl.supabase.co",
+  supabasePublishableKey: "sb_publishable_NoQM4G6viEmlS3H_XIqFNw_zuXyzR96",
 };
